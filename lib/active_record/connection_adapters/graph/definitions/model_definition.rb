360c2af81b7484a557309611bc8b5ee78883b358
Nikhil Lanjewar <nikhil@lanjewar.com>
2012-02-13 05:00:45 +0530

    Updated model_definition

--- lib/active_record/connection_adapters/graph/definitions/model_definition.rb
+++ lib/active_record/connection_adapters/graph/definitions/model_definition.rb
@@ -1,24 +1,32 @@
-class ActiveRecord::ConnectionAdapters::Graph::Definitions::ModelDefinition
-  def initialize(model_name)
-    @model_name = model_name.to_s
-    @columns = {}
-  end
+module ActiveRecord
+  module ConnectionAdapters
+    module Graph
+      module Definitions
+        class ModelDefinition
+          def initialize(model_name)
+            @model_name = model_name.to_s
+            @columns = {}
+          end
 
-  def column(column_name, column_type, options={})
-    @columns[column_name.to_s] = column_type.to_s
-  end
+          def column(column_name, column_type, options={})
+             @columns[column_name.to_s] = column_type.to_s
+          end
 
-  def timestamps(*args)
-    options = args.extract_options!
-    column :created_at, :datetime, options
-    column :updated_at, :datetime, options
-  end
+          def timestamps(*args)
+            options = args.extract_options!
+            column :created_at, :datetime, options
+            column :updated_at, :datetime, options
+          end
 
-  def to_hash
-    return_hash = {}
-    return_hash['model'] = @model_name
-    return_hash.merge! @columns
-
-    return_hash
+          def to_hash
+            return_hash = {}
+            return_hash['model'] = @model_name
+            return_hash.merge! @columns
+        
+            return_hash
+          end
+        end
+      end
+    end
   end
 end
--- lib/activerecord-neo4j-adapter.rb
+++ lib/activerecord-neo4j-adapter.rb
@@ -3,7 +3,6 @@ require "activerecord-neo4j-adapter/version"
 module Activerecord
   module Neo4j
     module Adapter
-      # Your code goes here...
     end
   end
 end
