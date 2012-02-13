b65abb79d09327255097a78290895fa945c53761
Nikhil Lanjewar <nikhil@lanjewar.com>
2012-02-13 20:58:03 +0530

    Added drop_table and add_index

--- lib/active_record/connection_adapters/neo4j/sql/database_statements.rb
+++ lib/active_record/connection_adapters/neo4j/sql/database_statements.rb
@@ -7,11 +7,14 @@ module ActiveRecord
       module Sql
         module DatabaseStatements
           def tables(name=nil, database=nil)
-            table_names = neo_server.find_node_index indices[:model], 'type', 'model'
+            table_names = neo_server.find_node_index(indices[:model], 'type', 'model').collect{|node| node['data']['model']} rescue nil
             table_names || []
           end # tables
 
           def create_table(model_name, options={})
+            
+            raise ArgumentError.new("Model #{model_name} already exists!") if table_exists?(model_name)
+
             model_definition = ActiveRecord::ConnectionAdapters::Graph::Definitions::ModelDefinition.new model_name
 
             yield model_definition if block_given?
@@ -20,6 +23,48 @@ module ActiveRecord
             
             neo_server.add_node_to_index indices[:model], 'type', 'model', model_node
             neo_server.add_node_to_index indices[:model], 'model', model_name, model_node
+          end # create_table
+
+          def drop_table(model_name, options={})
+            # Get the model node
+            model_node = get_model_node model_name
+
+            # Remove model node from indices
+            neo_server.remove_node_from_index indices[:model], 'type', 'model', model_node
+            neo_server.remove_node_from_index indices[:model], 'model', model_name, model_node
+
+            # Delete the node
+            model_node.del
+          end # drop_table
+
+          def add_index(model_name, column_name, options={})
+            # Get model node
+            model_node = get_model_node model_name
+
+            # Check if index exists on the model
+            raise ArgumentError.new("Index for #{column_name.inspect} exists on model #{model_name}")\
+             if model.indices.present? and (model_node.indices & column_index_name_for(column_name)).present?
+
+            # Add new indices
+            model_indices = model_node.respond_to?(:indices) ? model_node.indices : []
+            model_indices += column_index_name_for(column_name)
+
+            # Update model with new indices
+            neo_server.set_node_properties model_node, {'indices' => model_indices}
+          end # add_index
+
+          protected
+          def get_model_node(model_name)
+            model_node_attributes = neo_server.find_node_index indices[:model], 'model', model_name
+            raise ArgumentError.new("Model #{model_name} does not exist!") if not model_node_attributes.present?
+            model_node_id = model_node_attributes.first['self'].split('/').last.to_i
+            model_node = Neography::Node.load(model_node_id)
+          end
+
+          def column_index_name_for(column_name)
+            column_names = Array.wrap(column_name)
+
+            column_names.collect{|column_name| 'column_'+column_name.to_s}
           end
         end # DatabaseStatements
       end # Sql
