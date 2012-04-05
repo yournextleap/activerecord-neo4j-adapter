965e34e1af43921a1e33e1b5a2bfd20a105f3834
Nikhil Lanjewar <nikhil@lanjewar.com>
2012-04-05 12:26:27 +0530

    Added an illustrator class for gaining raw access to a Neo4j REST server

--- lib/activerecord-neo4j-adapter.rb
+++ lib/activerecord-neo4j-adapter.rb
@@ -1,4 +1,5 @@
 require "activerecord-neo4j-adapter/version"
+require "activerecord-neo4j-adapter/examples/neo4j_rest_illustrator"
 require 'active_record/hybrid_database_connector'
 require 'active_record/connection_adapters/neo4j/graph_methods'
 require 'active_record/relation/graph_methods'
